183504778ceed65f5bfe950d8def258d52e78164
Nick Harding <100789789+colonelkablam@users.noreply.github.com>
2024-11-20 11:14:12 +0000

    minor formatting and adding About page content

--- src/header.js
+++ src/header.js
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="header">
       <nav className="nav">
-      <Link to="/" className="nav-link">
+        <Link to="/" className="nav-link">
           Home
         </Link>
         <Link to="/about" className="nav-link">
